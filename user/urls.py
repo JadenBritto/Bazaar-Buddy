8023910baae19c191e416ab407060ba2c4a14bb2
Elson Machado <elsonmachadoem@gmail.com>
2025-07-26 12:32:56 +0530

    test

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('signup/', views.register, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
